c61209f3d9e533a5ceb7b91e6e8fe3a2317e7007
Dhairya-1323 <chabikant712@gmail.com>
2026-04-30 19:46:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -491,6 +491,8 @@ Which one interests you?`
   });
 });
 
-app.listen(5000, () =>
-  console.log("Server running on http://localhost:5000")
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
