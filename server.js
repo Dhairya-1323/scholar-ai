7aebb8e2b6329c56894ebb9fd2b41f0aa5dfada0
Dhairya-1323 <chabikant712@gmail.com>
2026-05-02 01:34:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend running ✅");
+});
 
 // ================= STREAM =================
 function streamResponse(res, text) {
